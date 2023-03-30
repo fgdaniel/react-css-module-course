5439b4d442b58f05eac9805e99546238a71903b0
fgdaniel <mibercsk94@gmail.com>
2023-03-30 14:26:19 +0300

    Add validation state

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,20 +6,47 @@ import "./CourseInput.css";
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState("");
 
+	/* Folosim state pentru validarea inputului, default fiind validat 
+	 si checkuim dupa daca este valid sau nu */
+	const [isValid, setIsValid] = useState(true);
+
 	const goalInputChangeHandler = (event) => {
+		/* Resetam validarea la varianta originala atunci cand userul scrie in input */
+		if (event.target.value.trim().length > 0) {
+			setIsValid(true);
+		}
 		setEnteredValue(event.target.value);
 	};
 
 	const formSubmitHandler = (event) => {
 		event.preventDefault();
+		if (enteredValue.trim().length === 0) {
+			/* Daca utilizatorul a introdus o valoare goala atunci nu ii validam inputul 
+			si nu adaugam itemul gol absolut deloc */
+			setIsValid(false);
+			return;
+		}
 		props.onAddGoal(enteredValue);
 	};
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className="form-control">
-				<label>Course Goal</label>
-				<input type="text" onChange={goalInputChangeHandler} />
+			<div className={`form-control ${!isValid ? "invalid" : ""}`}>
+				<label
+					style={{
+						color: !isValid ? "red" : "",
+					}}
+				>
+					Course Goal
+				</label>
+				<input
+					type="text"
+					onChange={goalInputChangeHandler}
+					style={{
+						borderColor: !isValid ? "red" : "",
+						backgroundColor: !isValid ? "salmon" : "",
+					}}
+				/>
 			</div>
 			<Button type="submit">Add Goal</Button>
 		</form>
